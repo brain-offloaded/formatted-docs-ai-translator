65616f5fdb466a44c63fc9333ada96f8e8230648
leejs1030 <leejs1030@korea.ac.kr>
2025-04-10 23:11:37 +0900

    csv parser options 제작

---
+++ src/nest/parser/dto/options/csv-parser-options.dto.ts
@@ -0,0 +1,6 @@
+import { BaseParseOptionsDto } from '../base-parse-options.dto';
+
+export class CsvParserOptionsDto extends BaseParseOptionsDto {
+  delimiter: string;
+  replaceDelimiter: string;
+}
